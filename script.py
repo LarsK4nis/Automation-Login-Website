5ef8dc142edfff68db0cf4323ff8c3156dcc035f
0t4peo <tommy00HKT@gmail.com>
2022-07-18 08:28:19 +0200

    Code Cleaning
    Libraries elimination and comment lines

--- script.py
+++ script.py
@@ -1,25 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.action_chains import ActionChains 
-from selenium.webdriver import ActionChains
-
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
 import yaml
 import requests 
 import time
 
-request = requests.get('http://90.174.128.109:8080/SymphonyWeb/Account/Login')
-configuration = yaml.full_load(open('detalles.yml'))
+request = requests.get('WEBPAGE__CHOSEN') # WEB PAGE TARGET
+configuration = yaml.full_load(open('YOUR_INFORMATION.yml')) # USER INFORMATION ACCOUNT .YML
 usuario = configuration ['data_user']['user']
 passwd = configuration ['data_user']['password']
 
-driver = webdriver.Chrome()
-css1 = ".login .content .form-actions .btn-success"
+driver = webdriver.Chrome() # BROWSER CHOSEN 
+css1 = ".login .content .form-actions .btn-success" # CSS selector used for button 
 
-def login(url,usernameOrEmailAddress, username, password, password1):
+def login(url,usernameOrEmailAddress, username, password, password1): 
    driver.get(url)
    driver.find_element(By.NAME, usernameOrEmailAddress).send_keys(username)
    driver.find_element(By.NAME, password).send_keys(password1)
@@ -29,7 +22,7 @@ def login(url,usernameOrEmailAddress, username, password, password1):
 if(request.status_code == 200):
    login ("http://90.174.128.109:8080/SymphonyWeb/Account/Login", "usernameOrEmailAddress", usuario, "password", passwd)
    time.sleep(3)
-   driver.find_element(By.LINK_TEXT,'Usuario').click() # LOCALIZAMOS Y CLICKAMOS EN USUARIO
+   driver.find_element(By.LINK_TEXT,'Usuario').click() # LOOK FOR ELEMENT USER AND CLICK ON IT
    objetivoFichar = driver.find_element(By.LINK_TEXT,'Fichar').click() # DESPLEGAMOS DROP-MENU, LOCALIZAMOS FICHAR Y CLICKAMOS
    time.sleep(3)
    fichajeEstrella = driver.find_element(By.LINK_TEXT,'FICHAR').click() # CLICKAMOS EN FICHAR Y SE ACTUALIZA ESTADO PAR/IMPAR
